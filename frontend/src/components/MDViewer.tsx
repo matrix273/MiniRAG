47bbb1b1708765a4ea01a8b30995057aab0827f6
matrix273 <matrix273@gmail.com>
2026-06-04 01:22:07 +0800

    fix: MDViewer Spin tip warning

--- frontend/src/components/MDViewer.tsx
+++ frontend/src/components/MDViewer.tsx
@@ -61,7 +61,7 @@ export default function MDViewer({ fileUrl, docId }: MDViewerProps) {
         height: '100%',
         padding: 20 
       }}>
-        <Spin tip="Loading..." />
+        <Spin tip="Loading..." fullscreen />
       </div>
     )
   }
